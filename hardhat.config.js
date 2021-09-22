d3c1cf7bf50e367dbbacbece0862119d154acfcd
lucadonnoh <donnoh99@gmail.com>
2021-09-22 11:24:08 +0200

    hardhat-etherscan configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -18,4 +20,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.7",
+  networks: {
+    hardhat: {},
+    ropsten: {
+      url: process.env.ALCHEMY_ROPSTEN_API_URL
+    }
+  },
+  etherscan: {
+    // API key for your Etherscan account
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
 };
