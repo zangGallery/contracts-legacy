14372850e505676ec83c128a3c4d0e0b2d8bfaf2
Samuele Marro <marrosamuele@gmail.com>
2021-12-22 18:45:45 +0100

    Added meeting notes.

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -44,8 +44,6 @@ contract zangMarketplace is Pausable, Ownable {
 
     // a token can have multiple listings
     mapping(uint256 => Listing[]) public listings;
-    // how many of a token are listed
-    mapping(uint256 => uint256) public amountListed;
 
     constructor(IZangNFT _zangNFTAddress, address _ZangCommissionAccount) {
         ZangNFTAddress = _zangNFTAddress;
@@ -53,11 +51,10 @@ contract zangMarketplace is Pausable, Ownable {
     }
 
     function listToken(uint256 _tokenId, uint256 _price, uint256 _amount) public whenNotPaused {
-        require(_amount <= ZangNFTAddress.balanceOf(msg.sender, _tokenId), "Not enough tokens to list");
+        require(_amount <= ZangNFTAddress.balanceOf(msg.sender, _tokenId), "Not enough tokens to list"); // Opz.
         require(_price > 0, "Price must be greater than 0");
 
         listings[_tokenId].push(Listing(_price, msg.sender, _amount));
-        amountListed[_tokenId] += _amount;
         emit TokenListed(_tokenId, msg.sender, _amount, _price);
     }
 
@@ -74,14 +71,16 @@ contract zangMarketplace is Pausable, Ownable {
 
     function _delistToken(uint256 _tokenId, uint256 _listingIndex) private {
         _removeListing(_tokenId, _listingIndex);
-        amountListed[_tokenId] -= listings[_tokenId][_listingIndex].amount;
         emit TokenDelisted(_tokenId);
     }
 
     function _handleFunds(uint256 _tokenId, address seller) private {
-        uint256 platformFee = msg.value * platformFeePercentage / 10000;
+        // TODO: Platform fee + Zang commission must not go over 100%
+        // TODO: Test integer division rounding errors
+        uint256 platformFee = (msg.value * platformFeePercentage) / 10000;
         (address creator, uint256 creatorFee) = ZangNFTAddress.royaltyInfo(_tokenId, msg.value);
         uint256 sellerEarnings = msg.value - platformFee - creatorFee;
+        // Test: The sum of three of them must be equal to msg.value
         payable(ZangCommissionAccount).transfer(platformFee);
         payable(creator).transfer(creatorFee);
         payable(seller).transfer(sellerEarnings);
@@ -90,6 +89,7 @@ contract zangMarketplace is Pausable, Ownable {
     function buyToken(uint256 _tokenId, uint256 _listingIndex, uint256 _amount) public payable whenNotPaused {
         require(listings[_tokenId].length > _listingIndex, "Listing index out of bounds");
         require(listings[_tokenId][_listingIndex].seller != msg.sender, "Cannot buy from yourself");
+        // TODO: Invert for clarity
         require(listings[_tokenId][_listingIndex].amount >= _amount, "Not enough tokens to buy");
         address seller = listings[_tokenId][_listingIndex].seller;
         // if seller transfers tokens "for free", their listing is still active! if they get them back they can still be bought
@@ -102,7 +102,9 @@ contract zangMarketplace is Pausable, Ownable {
         _handleFunds(_tokenId, seller);
 
         ZangNFTAddress.safeTransferFrom(seller, msg.sender, _tokenId, _amount, "");
+        // TODO: Delist only if there are no more tokens left
         _delistToken(_tokenId, _listingIndex);
+        // TODO: Decrease listing amount
 
         emit TokenPurchased(_tokenId, msg.sender, seller, _amount, price);
     }
